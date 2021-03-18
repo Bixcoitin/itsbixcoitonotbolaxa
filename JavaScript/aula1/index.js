e969bf5e23312b98110c5610e5783342788001c6
bix <celsoemilioleite@gmail.com>
2021-03-18 20:24:07 -0300

    numbers and identation

--- JavaScript/aula1/index.js
+++ JavaScript/aula1/index.js
@@ -1,2 +1,8 @@
-console.log("Hello, Vadim!");
-console.log('Hello, Blyat');
+console.log("Hello, Vadim!"); //strings (já sabia)
+console.log("Hello, Blyat"); // aspas simples e duplas só servem para a identação mais qualificada do texto "''" '""'
+console.log("'Hello', Bag!");
+
+// Python é melhor com números que JavaScript
+console.log(123); //int (tbm já sabia)
+console.log(2.3); //float (já sabia)
+console.log(2, 2.1); // a , serve para dar uma identação sem quebra de linha
