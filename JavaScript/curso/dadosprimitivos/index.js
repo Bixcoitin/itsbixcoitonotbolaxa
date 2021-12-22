fde4a1da5a1d8ce2855c2b90c27dabeb1def8961
Bixcoitin <celsoemilioleite@gmail.com>
2021-12-22 14:12:30 -0300

    alterações e volta ao curso de js

---
+++ JavaScript/curso/dadosprimitivos/index.js
@@ -0,0 +1,11 @@
+// String, number, undefined
+
+const nome = 'Dante'; //string
+const nome1 = "Dante"; //string
+const nome2 = `Dante`; //string
+
+const num = 10; //number
+const num1 = 10.52; // number
+
+let nomeAluno; // undefined = don`t apoint in any place in memory
+let sobrenomeAluno = null; // Nulo -> don`t apoint in any place in memory, but isn`t the same as undefined
