036434fe2b3d1539c564e5f9aff678476685813f
Dante Alighieri <dantealileite@gmail.com>
2022-06-19 17:27:45 -0300

    terminando

--- JavaScript/curso/aula13/ex/main.js
+++ JavaScript/curso/aula13/ex/main.js
@@ -2,13 +2,16 @@ const nome = prompt("What's your name?");
 //document.write("One text"); | windows.document.write();
 
 const nameSplited = nome.split(' ');
-const lenth = nameSplited.length
+const lenth = nome.length
+const secondLetter = nome.charAt(1);
+const firstA = nome.search('a');
+const lastA = nome.lastIndexOf('a');
 
 document.body.innerHTML = `Your name is: ${nome}<br />`;
 document.body.innerHTML += `Your name have ${lenth} letters<br />`;
-document.body.innerHTML += `The second letter of your name is: <br />`;
-document.body.innerHTML += `The first indice of the letter \'a\' on your name: <br />`;
-document.body.innerHTML += `The last indice of the letter \'a\' on your name: <br />`;
+document.body.innerHTML += `The second letter of your name is: ${secondLetter} <br />`;
+document.body.innerHTML += `The first indice of the letter \'a\' on your name: ${firstA} <br />`;
+document.body.innerHTML += `The last indice of the letter \'a\' on your name: ${lastA} <br />`;
 document.body.innerHTML += `The last 3 letters of your name are: <br />`;
 document.body.innerHTML += `The words in your name are: ${nameSplited}<br />`;
 document.body.innerHTML += `Your name in upper case letters: <br />`;
