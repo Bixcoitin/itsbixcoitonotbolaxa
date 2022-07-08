a63636d1bc69ba9e07c3446fed84f8e399f5958a
dantealighierii <dantealileite@gmail.com>
2022-07-07 21:31:04 -0300

    Finalizei o exercicio de JavaScript

--- JavaScript/curso/aula13/ex/main.js
+++ JavaScript/curso/aula13/ex/main.js
@@ -1,18 +1,25 @@
 const nome = prompt("What's your name?");
 //document.write("One text"); | windows.document.write();
 
+const nomeR = nome.replace(/ /, '');
+const nomeT = nome.trim();
 const nameSplited = nome.split(' ');
-const lenth = nome.length
-const secondLetter = nome.charAt(1);
-const firstA = nome.search('a');
-const lastA = nome.lastIndexOf('a');
+const lengthR = nomeR.length;
+const length = nome.length;
+const lengthT = nomeT.length;
+const nameSliced = nomeT.slice(-3); // se vc n especificar um final ele seta como a string inteira
+const secondLetter = nome[1]; // pode ser tbm nome.charAt(1);
+const firstA = nome.indexOf('i');
+const lastA = nome.lastIndexOf('i');
+const upperCase = nome.toUpperCase();
+const lowerCase = nome.toLowerCase();
 
 document.body.innerHTML = `Your name is: ${nome}<br />`;
-document.body.innerHTML += `Your name have ${lenth} letters<br />`;
+document.body.innerHTML += `Your name have ${lengthR} letters<br />`;
 document.body.innerHTML += `The second letter of your name is: ${secondLetter} <br />`;
-document.body.innerHTML += `The first indice of the letter \'a\' on your name: ${firstA} <br />`;
-document.body.innerHTML += `The last indice of the letter \'a\' on your name: ${lastA} <br />`;
-document.body.innerHTML += `The last 3 letters of your name are: <br />`;
+document.body.innerHTML += `The first indice of the letter \'i\' on your name: ${firstA} <br />`;
+document.body.innerHTML += `The last indice of the letter \'i\' on your name: ${lastA} <br />`;
+document.body.innerHTML += `The last 3 letters of your name are: ${nameSliced}<br />`;
 document.body.innerHTML += `The words in your name are: ${nameSplited}<br />`;
-document.body.innerHTML += `Your name in upper case letters: <br />`;
-document.body.innerHTML += `Your name in lower case letters: <br />`;
+document.body.innerHTML += `Your name in upper case letters: ${upperCase} <br />`;
+document.body.innerHTML += `Your name in lower case letters: ${lowerCase} <br />`;
